e2be5db074d7f7c689b4438c43f3aec4e86cdd31
M Natoli <natoli.missy@gmail.com>
2022-02-02 10:43:19 -0600

    changed fonts

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -6,8 +6,8 @@ function Jumbotron() {
             <div>
                 <div className="jumbotron py-5">
                     <div className="row no-gutters">
-                        <div className="col-6 px-5">
-                            <h1 className="display-4 title-font">
+                        <div className="col-6 px-2">
+                            <h1 className="display-3 title-font">
                                 <strong>Hoppin' Around</strong>
                             </h1>
                             <h2 className="lead subtitle-font">
